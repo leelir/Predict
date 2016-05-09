36ee8477b44c7e09eb467273d84b3cd2a81b0875
leelir <leelir7@hotmail.com>
2016-05-09 09:54:46 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Predict' do
+source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+pod 'Material', '~> 1.0'
+end
---
+++ Predict/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Predict
+//
+//  Created by 张碧贤 on 16/5/7.
+//  Copyright © 2016年 zs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
